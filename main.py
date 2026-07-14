4599128709a2f53ef4770a8d7279484f9b5c52f7
Rohith <rohithunairaus@gmail.com>
2026-07-14 22:12:02 +1000

    Drone rotate commands

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ drone = Drone()
 drone.connect()
 drone.take_off()
 
-drone.move_forward(80)
-time.sleep(2)
-drone.move_backward(50)
-time.sleep(2)
+#drone.move_forward(80)
+#time.sleep(2)
+#drone.move_backward(50)
+#time.sleep(2)
+
+drone.rotate(5)
 
 drone.land()
 time.sleep(1)
