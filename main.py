22c09ba3fe24c0788a64d31736016dcc36588098
Rohith <rohithunairaus@gmail.com>
2026-07-14 22:04:04 +1000

    Drone up and down commands

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 from pysimverse import Drone
 import time
+
+#initialization
 drone = Drone()
 drone.connect()
-
 drone.take_off()
-time.sleep(3)
+
+drone.move_down(20)
+time.sleep(2)
+drone.move_up(30)
+time.sleep(2)
+
 drone.land()
 time.sleep(1)
